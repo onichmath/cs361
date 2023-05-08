86c4c4ac7171917a2869b0dfe88c50ac53a79af6
Matthew <mathonich@yahoo.com>
2023-05-08 11:16:05 -0700

    Client TODO

---
+++ trail_microservice/client.py
@@ -0,0 +1,20 @@
+"""
+Created by: Matthew O'Malley-Nichols
+Code based off of: https://zeromq.org/languages/python/
+"""
+
+import zmq
+
+context = zmq.Context()
+print("Connecting to server")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+while True:
+    # Send requests to db
+    # TODO: SQLless queries
+    request = input("Send a SQL request to server: ")
+    socket.send(request)
+
+    return_message = socket.recv()
+    print(f"Returned: {return_message}")
