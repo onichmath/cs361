edbf976d3b8fa6dbb12309c2d61adf5afe7b9a3c
Matthew <mathonich@yahoo.com>
2023-05-22 16:29:01 -0700

    Fixed errors in add filehashtodbb

--- pyhash/database_operations.py
+++ pyhash/database_operations.py
@@ -1,16 +1,17 @@
 import hash_file 
-from questions import question_single_file_path
+from questions import question_single_file_path, get_filename_from_path, question_confirm
+from database import client
 
 def add_file_hash_to_database():
     while True:
         path = question_single_file_path()
         try:
-            blake_hash = get_single_file_hash(path)
+            blake_hash = hash_file.hash_file_from_absolute_path(path)
             file_name = get_filename_from_path(path)
         except:
             print(FileNotFoundError("File not found"))
-            if questionary.confirm("Try another path?").ask():
-               continue 
+            if question_confirm("Try another path?"):
+                continue
             return
         try:
             #query = f"INSERT INTO hashes (hash,file_name) VALUES ('{blake_hash.hexdigest()}','{file_name}') WHERE NOT EXISTS (SELECT * FROM hashes WHERE file_name = '{file_name}' AND hash = '{blake_hash.hexdigest()}')"
